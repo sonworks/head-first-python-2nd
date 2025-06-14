6766b7bb453710ab8a0b29fa439ed0c5dd1d0671
sonworks <csw0413@outlook.com>
2025-06-14 16:26:28 +0900

    Chapter 3

---
+++ ch03-구조화된데이터/dictionary.py
@@ -0,0 +1,13 @@
+#dictionary.py
+
+person3 = { 'Name': 'Ford Prefect',
+            'Gender': 'Male',
+            'Occupation': 'Researcher',
+            'Home Planet': 'Betelgeuse Seven' }
+
+
+print(person3)
+print('Name is ' + person3['Name'])
+
+person3['Age'] = 33
+print(person3)
